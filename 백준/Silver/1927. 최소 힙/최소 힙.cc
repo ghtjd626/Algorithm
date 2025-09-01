cc2c4916bdc323c8af47f6b594f0aa8adbd31094
KeunHoJeon <68725481+ghtjd626@users.noreply.github.com>
2025-09-02 00:15:31 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main () {
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    while(n--){
+        int x;
+        cin >> x;
+        if(x == 0){
+            if(pq.empty()) cout << '0' << '\n';
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else{
+            pq.push(x);
+        }
+    }
+}
